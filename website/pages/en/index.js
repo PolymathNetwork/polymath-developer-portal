efa98711b8419e5ad57b0ba535f0b170b0e1e530
Shannon Clarke <shannonjamalclarke@gmail.com>
2018-07-19 05:43:03 -0400

    Fix typos in copy on front page

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -105,7 +105,7 @@ const Features = props => (
         content: 'Core Ethereum Smart Contracts for Polymath',
         image: imgUrl('settings.svg'),
         imageAlign: 'top',
-        title: 'Solidity Docs', 
+        title: 'Solidity Docs',
         infoLink: 'https://github.com/PolymathNetwork/polymath-core',
       },
       {
@@ -126,46 +126,46 @@ const Features = props => (
 
 const FeatureCallout = props => (
   <div
-    className = "productShowcaseSection launchSection paddingBottom"
-    style={{textAlign: 'center', background: '#404e87'}}>
-    < h2 > Launch Securities Token in Minutes.<br/> <span className="launchSection-raise">Raise Funds</span> </h2>
+    className="productShowcaseSection launchSection paddingBottom"
+    style={{ textAlign: 'center', background: '#404e87' }}>
+    < h2 > Launch Securities Token in Minutes.<br /> <span className="launchSection-raise">Raise Funds</span> </h2>
   </div>
 );
 
 const FeatureWork = props => (
   <div
-    className = "workSection"
-    style={{textAlign: 'center'}}>
+    className="workSection"
+    style={{ textAlign: 'center' }}>
     < h1 className="workSection-heading"> How Polymath Works</h1>
-    < div className = "workSection-details" > An interface between financial securities and the blockchain, simplifying the process for issuers to overcome the complex technical and legal challenges token creation. </div>
+    < div className="workSection-details" > An interface between financial securities and the blockchain, simplifying the process for issuers to overcome the complex technical and legal challenges token creation. </div>
     <button className="button btn-blue">GET STARTED</button>
     <div className="workSection-tab">
       <div className="workSection-tab-row">
-        < div className = "workSection-tab-col terminal">
-          <img src="/img/Terminal_1.svg" className="wrokSection-img"/>
+        < div className="workSection-tab-col terminal">
+          <img src="/img/Terminal_1.svg" className="wrokSection-img" />
         </div>
-        < div className = "workSection-tab-col terminal" >
-          <img src="/img/Terminal_2.svg" className="wrokSection-img"/>
+        < div className="workSection-tab-col terminal" >
+          <img src="/img/Terminal_2.svg" className="wrokSection-img" />
         </div>
       </div>
       <div className="workSection-tab-row">
-        < div className = "workSection-tab-col">
-          < div className = "workSection-tab-col-heading"> modules / STO / ISTO.sol </div>
+        < div className="workSection-tab-col">
+          < div className="workSection-tab-col-heading"> modules / STO / ISTO.sol </div>
           <div className="workSection-tab-col-content">Interface to be implemented by all STO modules. This contract is used to verify the investment, whether the investor provide the allowance to the STO or not.</div>
         </div>
-        < div className = "workSection-tab-col">
-          < div className = "workSection-tab-col-heading">contracts / interfaces / IST20.sol</div>
-          < div className = "workSection-tab-col-content" > This is the nterface for the ST20 token standard. It mints new tokens and assigns them to the target _investor. It also enables a Burn function used to burn the securityToken.</div>
+        < div className="workSection-tab-col">
+          < div className="workSection-tab-col-heading">contracts / interfaces / IST20.sol</div>
+          < div className="workSection-tab-col-content" > This is the interface for the ST20 token standard. It mints new tokens and assigns them to the target investor. It also enables a Burn function used to burn the securityToken.</div>
         </div>
       </div>
       <div className="workSection-tab-row">
-        < div className = "workSection-tab-col">
-          < div className = "workSection-tab-col-heading"> contracts / SecurityTokenRegistry.sol </div>
-          <div className="workSection-tab-col-content">Registry contract for issuers to register their security tokens. Creates a new Security Token and saves it to the registry. You can add a new custom (Token should follow the ISecurityToken interface) Security Token and save it to the registry.</div>
+        < div className="workSection-tab-col">
+          < div className="workSection-tab-col-heading"> contracts / SecurityTokenRegistry.sol </div>
+          <div className="workSection-tab-col-content">Registry contract for issuers to register their security tokens. Creates a new Security Token and saves it to the registry. You can add a new custom Security Token (Token should follow the ISecurityToken interface) and save it to the registry.</div>
         </div>
-        < div className = "workSection-tab-col">
-          < div className = "workSection-tab-col-heading">contracts / GeneralPermissionManager.sol</div>
-          < div className = "workSection-tab-col-content" > This module allows issuers to define permissions for any actions or functions in any modules which are attached to their token, and then delegate those permissions to 3rd parties. Permissions are named and then defer to PermissionManager modules to check whether an address trying to perform an action has appropriate permission. </div>
+        < div className="workSection-tab-col">
+          < div className="workSection-tab-col-heading">contracts / GeneralPermissionManager.sol</div>
+          < div className="workSection-tab-col-content" > This module allows issuers to define permissions for any actions or functions in any modules which are attached to their token, and then delegate those permissions to 3rd parties. Permissions are named and then defer to PermissionManager modules to check whether an address trying to perform an action has appropriate permission. </div>
         </div>
       </div>
     </ div>
@@ -173,30 +173,30 @@ const FeatureWork = props => (
 );
 
 const FeatureWorth = props => (
-  <div className = "worthSection">
+  <div className="worthSection">
     <div className="worthSection-number">Over $1,000,000,000,000</div>
     <div className="worthSection-content">worth of digital assets powered by Polymath smart contracts</div>
     <div className="worthSection-companies">
-      <img src='/img/ipwe.png' className="worthSection-companies-img"/>
-      <img src='/img/corl.png' className="worthSection-companies-img"/>
-      <img src='/img/ipwe.png' className="worthSection-companies-img"/>
+      <img src='/img/ipwe.png' className="worthSection-companies-img" />
+      <img src='/img/corl.png' className="worthSection-companies-img" />
+      <img src='/img/ipwe.png' className="worthSection-companies-img" />
     </div>
     <div className="worthSection-seen">
       {/* <img src="/img/gitter.svg" className="worthSection-seen-logo gitter"/> */}
       <div className="worthSection-seen-icon">
-      <a href="https://gitter.im/PolymathNetwork/Lobby/" target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" className="worthSection-seen-logo gitter" viewBox="0 0 384 512">
-          <path d="M66.4 322.5H16V0h50.4v322.5zM166.9 76.1h-50.4V512h50.4V76.1zm100.6 0h-50.4V512h50.4V76.1zM368 76h-50.4v247H368V76z"/>
-        </svg>
-      </a>
+        <a href="https://gitter.im/PolymathNetwork/Lobby/" target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" className="worthSection-seen-logo gitter" viewBox="0 0 384 512">
+            <path d="M66.4 322.5H16V0h50.4v322.5zM166.9 76.1h-50.4V512h50.4V76.1zm100.6 0h-50.4V512h50.4V76.1zM368 76h-50.4v247H368V76z" />
+          </svg>
+        </a>
       </div>
       {/* <img src="/img/github.svg" className="worthSection-seen-logo github"/> */}
       <div className="worthSection-seen-icon">
-      <a href='https://github.com/PolymathNetwork/' target="_blank">
-        <svg xmlns="http://www.w3.org/2000/svg" className="worthSection-seen-logo github" viewBox="0 0 496 512">
-          <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"/>
-        </svg>
-      </a>
+        <a href='https://github.com/PolymathNetwork/' target="_blank">
+          <svg xmlns="http://www.w3.org/2000/svg" className="worthSection-seen-logo github" viewBox="0 0 496 512">
+            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+          </svg>
+        </a>
       </div>
     </div>
   </div>
@@ -212,8 +212,8 @@ class Index extends React.Component {
         <div className="mainContainer">
           <Features />
           <FeatureCallout />
-          <FeatureWork/>
-          < FeatureWorth/>
+          <FeatureWork />
+          < FeatureWorth />
         </div>
       </div>
     );
