434ef4f11b5609dfac82191198405d873e785fc7
Shannon Clarke <shannonajclarke@gmail.com>
2019-04-15 08:06:44 -0400

    Resolve issues with navbar links

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -42,7 +42,7 @@ const siteConfig = {
     { doc: "doc1", label: "Docs" },
     // { doc: "doc2", label: "Bug Bounty" },
     { doc: "doc4", label: "Tutorial" },
-    { doc: "interfaces_ISTO", label: "Contracts" },
+    { href: "/docs/version-2.1.0-interfaces_ISTO.html", label: "Contracts" },
     { page: "help", label: "Help" },
     {
       href: "https://blog.polymath.network/tech/home",
